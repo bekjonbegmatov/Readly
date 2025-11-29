3e48619d5bd0418f3071cc8ea2fb4de629058540
Bekhruz Begmatov <behruzbegmatov28@gmail.com>
2025-11-30 03:27:16 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,9 @@ set -e
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-# Start Gunicorn
-exec gunicorn server.wsgi:application --bind 0.0.0.0:8000 --workers 3
-
+# Start Gunicorn (через python -m, чтобы избежать проблем с PATH)
+exec python -m gunicorn server.wsgi:application \
+  --bind 0.0.0.0:8000 \
+  --workers 3 \
+  --access-logfile - \
+  --error-logfile - 
